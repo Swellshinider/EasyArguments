e6d2afbdabf99768a9c5cc7d082cc951e413a231
Swellshinider <eduardoleal.contact@gmail.com>
2024-12-07 13:44:05 -0300

    Refactor and enhance ArgumentsController functionality
    - Add documentation comments to ArgumentsController class and methods.
    - Rename ArgumentParser to ArgumentsController and refactor initialization.
    - Introduce Execute<T2> method to handle ArgumentExecutorAttribute.
    - Add ArgumentExecutorAttribute.cs to define ArgumentExecutorAttribute<T>.
    - Add ExecutableArgument.cs to define ExecutableArgument<T> struct.
    - Reorganize using directives in ArgumentsController.cs.

--- EasyArguments/ArgumentsController.cs
+++ EasyArguments/ArgumentsController.cs
@@ -1,21 +1,41 @@
 ﻿using EasyArguments.Attributes;
-using System.Reflection;
-using System.Linq;
 using EasyArguments.Exceptions;
-using System;
+using System.Linq;
+using System.Reflection;
 
 namespace EasyArguments;
 
-public class ArgumentParser<T> where T : new()
+/// <summary>
+/// Parses command-line arguments and maps them to the properties of a specified class.
+/// </summary>
+/// <typeparam name="T">
+/// The type of the class to which arguments will be mapped. 
+/// Must have a parameterless constructor and be annotated with <see cref="ArgumentsControllerAttribute"/>.
+/// </typeparam>
+/// <remarks>
+/// Initializes a new instance of the <see cref="ArgumentsController{T}"/> class with the specified arguments.
+/// </remarks>
+/// <param name="args">An array of command-line arguments to parse.</param>
+public class ArgumentsController<T>(string[] args) where T : new()
 {
-    private readonly string[] _args;
+    private readonly string[] _args = args;
     private const double Tolerance = 1e-10;
 
-    public ArgumentParser(string[] args)
-    {
-        _args = args;
-    }
-
+    /// <summary>
+    /// Parses the arguments and maps them to the properties of the type <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>
+    /// An instance of <typeparamref name="T"/> with its properties set according to the parsed arguments.
+    /// </returns>
+    /// <exception cref="MissingArgumentsControllerAttributeException">
+    /// Thrown when the type <typeparamref name="T"/> is not decorated with <see cref="ArgumentsControllerAttribute"/>.
+    /// </exception>
+    /// <exception cref="UnknownArgumentException">
+    /// Thrown when an argument is provided that does not match any property in <typeparamref name="T"/>.
+    /// </exception>
+    /// <exception cref="InvalidArgumentTypeException">
+    /// Thrown when an argument cannot be converted to the required type for a property in <typeparamref name="T"/>.
+    /// </exception>
     public T Parse()
     {
         var result = new T();
@@ -100,11 +120,28 @@ public class ArgumentParser<T> where T : new()
                     propInfo.SetValue(result, (Nullable<bool>)true);
                 else if (propInfo.PropertyType == typeof(bool))
                     propInfo.SetValue(result, true);
-                else 
+                else
                     throw new InvalidArgumentTypeException(inputArgument, typeof(bool));
             }
         }
 
         return result;
+    }
+
+    public IEnumerable<object?> Execute<T2>(T obj) where T2 : new()
+    {
+        var executableArguments = obj!.GetType().GetProperties()
+            .Where(p => p.GetCustomAttribute<ArgumentExecutorAttribute<T2>>() != null)
+            .Select(p => new ExecutableArgument<T2>(p, p.GetValue(obj), p.GetCustomAttribute<ArgumentExecutorAttribute<T2>>()!))
+            .ToList();
+
+        foreach (var execArg in executableArguments)
+        {
+            var executor = new T2();
+            var method = executor.GetType().GetMethod(execArg.Attribute.ExecutorName) 
+                ?? throw new MissingMethodException(execArg.Attribute.ExecutorName);
+
+            yield return method.Invoke(executor, [execArg.Value]);
+        }
     }
 }
---
+++ EasyArguments/Attributes/ArgumentExecutorAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyArguments.Attributes;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public sealed class ArgumentExecutorAttribute<T>(string methodName) : Attribute
+{
+    public string ExecutorName { get; } = methodName;
+}
