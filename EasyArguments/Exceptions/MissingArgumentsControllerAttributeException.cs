339515a93c240c4e0897c60f0643833336d92183
Swellshinider <eduardoleal.contact@gmail.com>
2025-01-25 13:53:02 -0300

    MissingArgumentsControllerAttributeException documentation

--- EasyArguments/Exceptions/MissingArgumentsControllerAttributeException.cs
+++ EasyArguments/Exceptions/MissingArgumentsControllerAttributeException.cs
@@ -7,6 +7,10 @@ namespace EasyArguments.Exceptions;
 /// </summary>
 public class MissingArgumentsControllerAttributeException : Exception
 {
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MissingArgumentsControllerAttributeException"/> class with a specified error message.
+	/// </summary>
+	/// <param name="type">The type that is missing the <see cref="ArgumentsControllerAttribute"/> attribute.</param>
 	public MissingArgumentsControllerAttributeException(Type type)
-		: base($"The target class '{type.Name}' must be decorated with the 'ArgumentsControllerAttribute' attribute.") { }
+		: base($"The target class '{type.Name}' must be decorated with the '{nameof(ArgumentsControllerAttribute)}' attribute.") { }
 }
