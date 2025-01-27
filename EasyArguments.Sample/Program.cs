a3f1384d41ee4b9c08606cf54026d1852fe9f10a
Swellshinider <eduardoleal.contact@gmail.com>
2025-01-27 12:55:17 -0300

    Add stop and start commands to argument parsing and execution

--- EasyArguments.Sample/Program.cs
+++ EasyArguments.Sample/Program.cs
@@ -2,11 +2,22 @@
 
 public static class Program
 {
-	public static void Main(string[] args)
+	public static bool IsRunning { get; set; } = true;
+	
+	public static void Main()
 	{
 		ArgumentsController.RedirectErrorToConsole = true;
-		var arguments = ArgumentsController.Parse<ArgumentsSample>(["-v", "-o=path_mec"]);
-		var results = ArgumentsController.Execute(arguments);
-		results.ToList().ForEach(Console.WriteLine);
+
+		while (IsRunning)
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write("> ");
+			var input = Console.ReadLine() ?? "";
+			Console.ResetColor();
+
+			var args = input.Split(' ', StringSplitOptions.TrimEntries);
+			var arguments = ArgumentsController.Parse<ArgumentsSample>(args);
+			_ = ArgumentsController.Execute(arguments).ToList();
+		}
 	}
 }
