3bcd4e6dad9d62c767b5c046713cbf063aa3ffc0
Swellshinider <eduardoleal.contact@gmail.com>
2025-02-03 08:19:42 -0300

    Refactor argument classes and add version display functionality

---
+++ EasyArguments.Sample/ValidateAndExecute.cs
@@ -0,0 +1,9 @@
+namespace EasyArguments.Sample;
+
+public static class ValidateAndExecute
+{
+	public static void DisplayVersion(bool shouldDisplay, string test)
+	{
+		Console.WriteLine("Version -> 10.0");
+	}
+}
